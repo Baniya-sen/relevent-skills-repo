01aa57b3fbb9cc6319565bcd5f30befd160031c8
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2023-09-30 18:07:22 +0530

    Update CaesarEncryption.java
    Added comments!

--- JAVA/CaesarEncryption.java
+++ JAVA/CaesarEncryption.java
@@ -18,10 +18,12 @@ public class CaesarEncryption {
 
         Scanner s = new Scanner(System.in);
 
+//         Checking if command line has only one argument i.e. the "Key"
         if (args.length != 1) {
             System.out.println("Usage: java \"class_name\" \"key\"");
             System.exit(1);
         } else {
+//            If command lne argument is indeed a number for key
             String argument1 = args[0];
             if (!argument1.matches("\\d+")) {
                 System.out.println("The provided key contains non-digit characters.");
@@ -31,10 +33,12 @@ public class CaesarEncryption {
 
         int key = Integer.parseInt(args[0]);
 
+//         Input
         System.out.print("Plaintext:  ");
         String plainText = s.nextLine();
 
         String cipherText = "";
+//         Printing Ciphertext character by character
         for (int i = 0; i < plainText.length(); i++) {
             char rotatedChar = rotate(plainText.charAt(i),key);
             cipherText += rotatedChar;
@@ -51,8 +55,12 @@ public class CaesarEncryption {
         char verifyCiphertext = toUpperCase(ciphertext);
 
         for (int i = 0; i < alphabetLength; i++) {
+
+//            If char is alphabet and present at ith index of Alpha
             if (verifyCiphertext == ALPHA[i]) {
                 if (isUpperCase(ciphertext)) {
+/*                    If key is >= 26 ith + key % 26(alphabetLength) will make sure
+                      that after 26th character, ith will go back to 0                             */
                     cipherT = ALPHA[(i + key) % alphabetLength];
                     break;
                 } else if (isLowerCase(ciphertext)) {
@@ -60,6 +68,7 @@ public class CaesarEncryption {
                     break;
                 }
             }
+//            [i % punctuations.length] is used so punctuation array will not go out of bounds because "for (i < 26)"
             else if (ciphertext == punctuations[i % punctuations.length]) {
                 cipherT = ciphertext;
             }
