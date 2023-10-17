4f60129cea19640355e85c06818385e6b8ba36ea
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2023-10-17 15:57:26 +0530

    Linear search and find on an Array

--- JAVA/SearchMaxIn_Array.java
+++ JAVA/SearchMaxIn_Array.java
@@ -1,18 +1,23 @@
+// Finding Max value in an Array using Linear search
+
+import java.util.Scanner;
+
 public class SearchMaxIn_Array {
     
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
+        int n_Elements = 0;
         
         do {
-            System.out.println("Number of Elements: ");
-            final int n_Elements = s.nextInt();
+            System.out.print("Number of Elements: ");
+            n_Elements = s.nextInt();
         } while (n_Elements < 1);
         
         final int[] array = new int[n_Elements];
         
         for (int i = 0; i < n_Elements; i++) {
-            System.out.println("Element " + i + " : ");
+            System.out.print("Element " + i + " : ");
             array[i] = s.nextInt();
         }
         
@@ -20,5 +25,13 @@ public class SearchMaxIn_Array {
     }
     
     public static int max(int[] array, int n) {
+        int highestValue = array[0];
+
+        for (int i = 1; i < n; i++) {
+            if (highestValue < array[i]) {
+                highestValue = array[i];
+            }
+        }
+        return highestValue;
     }
 }
