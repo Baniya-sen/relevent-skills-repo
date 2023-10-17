bf7499df085c85d40a9bb1c2b929bd95b8169901
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2023-10-17 15:11:07 +0530

    change to proper name

--- JAVA/Sorted_Temperature.java
+++ JAVA/Sorted_Temperature.java
@@ -1,44 +1,44 @@
-public class Average_Temperature {
+public class Sorted_Temperature {
     
     String city;
     int temperature;
     private static final int citiesLength = 10;
     
-    private Average_Temperature(String city, int temperature) {
+    private Sorted_Temperature(String city, int temperature) {
         this.city = city;
         this.temperature = temperature;
     }
     
     public static void main(String[] args) {
         
-        Average_Temperature[] temp = new Average_Temperature[citiesLength];
+        Sorted_Temperature[] temp = new Sorted_Temperature[citiesLength];
         
         System.out.println("Average July Temperatures by City");
         
         addCites(temp);
         sortTemp(temp);
         
-        for (Average_Temperature sorted_Temp : temp) {
+        for (Sorted_Temperature sorted_Temp : temp) {
             if (sorted_Temp != null) {
                 System.out.println(sorted_Temp.city + ": " + sorted_Temp.temperature);
             }
         }
     }
     
-    static void addCites(Average_Temperature[] temp) {
-        temp[0] = new Average_Temperature("New Delhi", 45);
-        temp[1] = new Average_Temperature("Bangalore", 37);
-        temp[2] = new Average_Temperature("Faridabad", 41);
-        temp[3] = new Average_Temperature("Guwahati", 34);
-        temp[4] = new Average_Temperature("Thiruvanthapuram", 37);
-        temp[5] = new Average_Temperature("Pune", 36);
-        temp[6] = new Average_Temperature("Ladakh", 30);
-        temp[7] = new Average_Temperature("Chandigarh", 39);
-        temp[8] = new Average_Temperature("Jaipur", 46);
-        temp[9] = new Average_Temperature("Noida", 40);
+    static void addCites(Sorted_Temperature[] temp) {
+        temp[0] = new Sorted_Temperature("New Delhi", 45);
+        temp[1] = new Sorted_Temperature("Bangalore", 37);
+        temp[2] = new Sorted_Temperature("Faridabad", 41);
+        temp[3] = new Sorted_Temperature("Guwahati", 34);
+        temp[4] = new Sorted_Temperature("Thiruvanthapuram", 37);
+        temp[5] = new Sorted_Temperature("Pune", 36);
+        temp[6] = new Sorted_Temperature("Ladakh", 30);
+        temp[7] = new Sorted_Temperature("Chandigarh", 39);
+        temp[8] = new Sorted_Temperature("Jaipur", 46);
+        temp[9] = new Sorted_Temperature("Noida", 40);
     }
     
-    static void sortTemp(Average_Temperature[] temp) {
+    static void sortTemp(Sorted_Temperature[] temp) {
         
         for (int i = 0; i < citiesLength; i++) {
             int firstIndexTemp = i;
