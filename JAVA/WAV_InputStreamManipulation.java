779c4d15c39d84c29f8acc1764fa8c546d298b56
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2023-10-19 22:21:13 +0530

    command line arguments verification

--- JAVA/WAV_InputStreamManipulation.java
+++ JAVA/WAV_InputStreamManipulation.java
@@ -2,5 +2,15 @@ public class WAV_InputStreamManipulation {
     
     public static void main(String[] args) {
         
+        if (args.length != 3) {
+            System.out.println("Usage: java WAV_InputStreamManipulation input.wav output.wav factor");
+            System.exit(1);
+        }
+
+        String isDecimal_Factor = args[2];
+        if (!isDecimal_Factor.matches("\\d+")) {
+            System.out.println("Usage: java WAV_InputStreamManipulation input.wav output.wav factor");
+            System.exit(1);
+        }
     }
 }
