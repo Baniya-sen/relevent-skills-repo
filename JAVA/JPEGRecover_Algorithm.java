894899d16592e7d173bf159eeaaf53b590b7cd05
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2023-10-23 22:25:39 +0530

    Algorithm to recover JPEG images from deleted memory

---
+++ JAVA/JPEGRecover_Algorithm.java
@@ -0,0 +1,14 @@
+public class JPEGRecover_Algorithm {
+    
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java FILENAME IMAGE");
+        }
+        
+        try {
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
