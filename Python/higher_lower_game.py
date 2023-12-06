7332ee644dd3e798eb9a8d44399f12c574c196bd
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2023-12-07 01:40:29 +0530

    Change loop variable assignment

--- Python/higher_lower_game.py
+++ Python/higher_lower_game.py
@@ -7,24 +7,25 @@ def main():
     score = 0
 
     # Randomly select data from data_dict
-    random_data_A = data[random_celeb()]
     random_data_B = data[random_celeb()]
 
     # Main game loop
     while True:
+        # a = b, so that in next loop, b becomes a and b get new data
+        random_data_A = random_data_B
+        random_data_B = data[random_celeb()]
+
         print(f"Compare A: {random_data_A['name']}, {random_data_A['description']}, from {random_data_A['country']}")
         print(vs)
         print(f"Compare B: {random_data_B['name']}, {random_data_B['description']}, from {random_data_B['country']}")
 
-        random_data_C = data[random_celeb()]
         choice = input("Who has more followers? Type 'A' or 'B': ").lower()
         clear()
 
-        # If user is right, swap a with b, and b with c, then continue game with score += 1
+        # If user is right, continue game with score += 1
         if check_followers(choice, random_data_A['follower_count'], random_data_B['follower_count']):
             score += 1
             print(f"\nYou're right! Your score: {score}")
-            random_data_A, random_data_B = random_data_B, random_data_C
         else:
             print("Wrong choice.. Game over!" if choice not in ('a', 'b') else f"\nYou Loose! Your score {score}")
             break
