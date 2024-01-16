f807a48b60c2fd98b0689426759bf86b58b79acc
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-01-16 18:16:42 +0530

    A simple weather app using HTML5, CSS and Javascript

---
+++ Javascript_Html_CSS/Weather_App/script.js
@@ -0,0 +1,41 @@
+const apiKey = "b2a0cf072e2a1d9876bd86f507e546a7";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+
+function titleCase(str) {
+    return str.replace(/\w\S*/g, function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+}
+
+async function getWeather (city) {
+    var response = await fetch(apiURL + `&q=${city}` + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        document.querySelector(".city-name").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        document.querySelector(".weather-icon").src = `images/${titleCase(data.weather[0].main)}.png`;
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector(".search button").addEventListener('click', ()=> {
+        let cityName = document.querySelector(".search input").value;
+
+        if (cityName) {
+            getWeather(cityName)
+        } else {
+            getWeather("delhi")
+        }
+    });
+});
