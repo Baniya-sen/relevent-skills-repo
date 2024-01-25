56e246cfcc165b62517a3633013199c463ffc6ea
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-01-25 17:24:19 +0530

    Update NLP_Fitness_Tracker.py
    Updated file to use pandas to store personal data of user in csv

--- Python/NLP_Fitness_Tracker.py
+++ Python/NLP_Fitness_Tracker.py
@@ -1,3 +1,4 @@
+import pandas
 import requests
 import datetime
 import os
@@ -8,19 +9,46 @@ NUTRITIONIX_APP_KEY = os.environ.get("NUTRITIONIX_APP_KEY")
 SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
 
 
+def personal_details():
+    personal_info = {}
+    try:
+        personal_file = pandas.read_csv("personal_data.csv")
+        # for data in personal_file:
+        personal_info["gender"] = personal_file["gender"].iloc[0]
+        personal_info["weight"] = personal_file["weight"].iloc[0]
+        personal_info["height"] = personal_file["height"].iloc[0]
+        personal_info["age"] = personal_file["age"].loc[0]
+
+    except FileNotFoundError:
+        personal_info["gender"] = input("What's your gender? [Male/Female]: ").lower()
+        while True:
+            try:
+                personal_info["weight"] = float(input("Weight in Kg: "))
+                personal_info["height"] = int(input("Height in cm: "))
+                personal_info["age"] = int(input("Your age: "))
+                break
+            except ValueError:
+                print("Enter valid input(s): ")
+                pass
+        pandas.DataFrame([personal_info]).to_csv("personal_data.csv", index=False)
+
+    return personal_info
+
+
 def exercise_details():
+    user_info = personal_details()
+
     nutritionix_headers = {
         "x-app-id": NUTRITIONIX_APP_ID,
         "x-app-key": NUTRITIONIX_APP_KEY,
         "Content-Type": "application/json",
     }
-
     nutritionix_data = {
         "query": input("What exercise did you performed? Duration?: "),
-        "gender": "male",
-        "weight_kg": 84,
-        "height_cm": 172,
-        "age": 25,
+        "gender": user_info["gender"],
+        "weight_kg": user_info["weight"],
+        "height_cm": user_info["height"],
+        "age": user_info["age"],
     }
 
     response = requests.post(NUTRITIONIX_ENDPOINT, headers=nutritionix_headers, json=nutritionix_data)
